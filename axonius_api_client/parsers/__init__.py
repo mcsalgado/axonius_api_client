886d55a9025f9e06d5b23a3ab05c0f4e203b40c2
Jim Olsen <jim@axonius.com>
2022-09-08 18:49:32 -0400

    wip for parsing dicts

--- axonius_api_client/parsers/__init__.py
+++ axonius_api_client/parsers/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Parsers for API models."""
-from . import config, fields, tables, url_parser, wizards
+from . import config, fields, parse_dicts, tables, url_parser, wizards
 
 __all__ = (
     "config",
@@ -8,4 +8,5 @@ __all__ = (
     "tables",
     "url_parser",
     "wizards",
+    "parse_dicts",
 )
